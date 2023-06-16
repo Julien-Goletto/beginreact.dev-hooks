0ebcd528d455e01c1af3895ac7bd2a3e04a2df53
Julien Goletto <julien.goletto@gmail.com>
2023-06-16 10:58:34 +0200

    :sparkles: 3-1 done

--- src/exercise/3-use-ref/03.jsx
+++ src/exercise/3-use-ref/03.jsx
@@ -1,19 +1,15 @@
 /* eslint-disable no-unused-vars */ // 🦁 Enlève cette ligne
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const useDebounce = (callback, time) => {
-  // 🦁 Remplace cette variable par un `useRef`
-  // 💡 timeout correspond à la référence de notre timeout.
-  //   Quand tu fais un setTimeout, il return une valeur que
-  //   tu peux clear afin de l'annuler. https://developer.mozilla.org/fr/docs/Web/API/setTimeout#valeur_de_retour
-  const timeout = null;
+
+  const debounce = useRef(null);
 
   const onDebounce = (...args) => {
-    // 🦁 Annule le timeout https://developer.mozilla.org/en-US/docs/Web/API/clearTimeout
-    // ℹ️ Cette fonction sera appelée à chaque fois que l'user tape un caractère, on veut donc clear
-    //    le dernier timeout pour relancer un nouveau timeout.
-    // 🦁 Crée un nouveau timeout https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
-    //    a la fin il doit appeler la callback avec les arguments et le temps est défini par le paramètre `time`
+    clearTimeout(debounce.current);
+    debounce.current = setTimeout(
+      () => callback(...args), 
+      time);
   };
 
   return onDebounce;
@@ -25,14 +21,12 @@ const fetchAgeByName = (name) => {
 
 const App = () => {
   const [result, setResult] = useState(null);
-
-  // 🦁 Wrap la function `onSearch` dans le hooks useDebounce
-  // 💡 const onSearch = useDebounce((value) => {...}, 500);
-  const onSearch = (value) => {
+  
+  const onSearch = useDebounce((value) => {
     fetchAgeByName(value).then((data) => {
       setResult(data);
     });
-  };
+  }, 500);
 
   return (
     <div>
