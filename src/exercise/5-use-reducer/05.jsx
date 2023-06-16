87b87f9a0543da365e0ca24221788c35875c1166
Julien Goletto <julien.goletto@gmail.com>
2023-06-16 15:24:02 +0200

    :sparkles: 5 - 4 New actions

--- src/exercise/5-use-reducer/05.jsx
+++ src/exercise/5-use-reducer/05.jsx
@@ -1,16 +1,42 @@
 import { useReducer } from 'react';
 
+const initialState = { value: 0 };
+
+const REDUCER_ACTIONS = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+  RESET: 'RESET',
+  PLUS_FIVE: 'PLUS_FIVE',
+  MINUS_FIVE: 'MINUS_FIVE',
+}
+
 const reducer = (state, action) => {
+
   switch (action.type) {
-    case 'INCREMENT':
+    case REDUCER_ACTIONS.INCREMENT:
       return { 
         ...state, 
         value: state.value + 1,
       };
-    case 'DECREMENT':
+    case REDUCER_ACTIONS.DECREMENT:
       return { 
         ...state, 
         value: state.value - 1,
+      };
+    case REDUCER_ACTIONS.RESET:
+      return { 
+        ...state, 
+        value: initialState.value,
+      };
+    case REDUCER_ACTIONS.PLUS_FIVE:
+      return { 
+        ...state, 
+        value: state.value + 5,
+      };
+    case REDUCER_ACTIONS.MINUS_FIVE:
+      return { 
+        ...state, 
+        value: state.value - 5,
       };
     default:
       return state;
@@ -18,13 +44,16 @@ const reducer = (state, action) => {
 };
 
 const Counter = () => {
-  const [state, dispatch] = useReducer(reducer, { value: 0 });
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
       <button>{state.value}</button>
       <button onClick={() => dispatch({ type: 'INCREMENT'})}>+</button>
+      <button onClick={() => dispatch({ type: 'PLUS_FIVE'})}>+ 5</button>
       <button onClick={() => dispatch({ type: 'DECREMENT'})}>-</button>
+      <button onClick={() => dispatch({ type: 'MINUS_FIVE'})}>- 5</button>
+      <button onClick={() => dispatch({ type: 'RESET'})}>Reset</button>
     </div>
   );
 };
